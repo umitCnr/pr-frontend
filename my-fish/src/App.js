1782cd13cac41e0f45be8795d9436d6dbf0671fa
umitcaner <cancar27127@gmail.com>
2024-10-02 06:22:32 +0300

    son

--- my-fish/src/App.js
+++ my-fish/src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
+import React from "react";
+import fish from './icons/fish-outline.svg';
+import starfish from './icons/icons8-starfish-50.png';
+import moss from './icons/seeding.svg';
+import WriteNew from "./text/newFish";
+import ocean from './icons/fish.png';
+
+
+
 
 function App() {
+
+
+
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+      <div className={'main-app'}>
+          <div className={'img'}>
+              <img className={'img-ocean'} />
+              <div>
+                  <img className={'img-fish'} src={ocean}/>
+              </div>
+              <div className="App">
+                  <h1 className={"baslik"} >My Fish</h1>
+                  <div className={"kind"}>
+                      <button  className={"bir"}><img className={'fish'} src={fish}/>Balıklar</button>
+                      <button className={"iki"}><img className={'starfish'} src={starfish}/>Omurgasız Canlılar</button>
+                      <button className={"uc"}><img className={'moss'} src={moss}/>Bitkiler</button>
+                  </div>
+              </div>
+              <div>
+                  <WriteNew/>
+              </div>
+          </div>
+      </div>
+
+
   );
 }
 
--- my-fish/src/text/newFish.js
+++ my-fish/src/text/newFish.js
@@ -1,39 +1,55 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import './write.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import fish from './../icons/fish.jpg';
+import omurgasiz from './../icons/omurgasiz.jpg';
+import herb from './../icons/yosun.jpg';
 
-const WriteNew =()=>{
-    const [text,setText] = useState("");
+const WriteNew = () => {
 
-    const text_write = () =>{
-        setText("Akvaryumda canlı beslemek, hem görsel bir şölen sunar hem de sakinleştirici bir hobi olabilir. Ancak, bu süreçte dikkat etmeniz gereken bazı önemli noktalar vardır. Yeni başlayanlar için temel bilgiler:\n" +
-            "\n" +
-            "Akvaryum Seçimi: Yeni başlayanlar için en az 20-30 litrelik bir akvaryum önerilir. Daha büyük akvaryumlar, su kalitesini korumayı daha kolay hale getirir. Cam akvaryumlar yaygın olarak tercih edilir, ancak akrilik akvaryumlar da hafif ve dayanıklıdır.\n" +
-            "\n" +
-            "Ekipman: Su akışını sağlamak ve suyun temizliğini korumak için bir filtre edinmelisiniz. Su sıcaklığını kontrol etmek için bir ısıtıcı kullanmalısınız. Çoğu tatlı su balığı 22-26°C arasında bir sıcaklıkta en iyi şekilde yaşar. Doğru aydınlatma, hem balıkların sağlığı hem de bitkilerin büyümesi için önemlidir. LED lambalar uzun ömürlü ve enerji tasarrufludur.\n" +
-            "\n" +
-            "Su Değişimi ve Kalitesi: pH, amonyak, nitrit ve nitrat seviyelerini düzenli olarak test edin. Su parametreleri, balıkların sağlığı için kritik öneme sahiptir. Haftada %10-20 oranında su değişimi yapmak, suyun kalitesini korumaya yardımcı olur.\n" +
-            "\n" +
-            "Balık Seçimi: Yeni başlayanlar için, barışçıl ve dayanıklı türler tercih edilmelidir. Neon tetra, guppy ve zebra danio gibi türler iyi seçeneklerdir. Yeni balıkları akvaryuma tanıtırken, öncelikle balıkları akvaryumun suyuyla tanıştırarak şok yaşamalarını önleyin.\n" +
-            "\n" +
-            "Beslenme: Balıklarınızı kaliteli yemlerle besleyin. Aşırı yemleme, suyun kirlenmesine ve balıkların sağlığının bozulmasına neden olabilir. Balık türlerine göre değişen farklı yemler kullanarak besin çeşitliliği sağlayın.\n" +
-            "\n" +
-            "Bitkiler ve Dekorasyon: Akvaryumda canlı bitkiler bulundurmak, suyun kalitesini artırır ve balıklar için doğal bir ortam yaratır. Taşlar, kayalar ve gizlenme alanları, balıklarınız için güvenli bölgeler sağlar.\n" +
-            "\n" +
-            "Akvaryum Bakımı: Akvaryum camlarını ve ekipmanları düzenli olarak temizleyin, ancak aşırı temizlikten kaçının. Doğal bakteri dengesinin bozulmaması önemlidir. Balıklarınızı düzenli olarak gözlemleyin. Davranışlarındaki değişiklikler, stres veya hastalık belirtisi olabilir.\n" +
-            "\n" +
-            "Akvaryumda canlı beslemek, sabır ve bilgi gerektiren bir süreçtir. Yukarıdaki ipuçlarını dikkate alarak, sağlıklı ve mutlu bir akvaryum ortamı yaratabilir, bu keyifli hobinin tadını çıkarabilirsiniz. Unutmayın, her akvaryum farklıdır ve zamanla öğrenmeye devam edeceksiniz!")
-    }
-useEffect(()=>{
-    text_write();
-})
 
-return(
-  <div>
-      <div className={"text-div"}>
-          <h1 className={"baslik"}>yeni başlayanlar için</h1>
-         <p className={"text"}>{text}</p>
-      </div>
-  </div>
-);
-}
+
+    return (
+        <div className={"main"}>
+            <div className={'sub-main'}>
+                <div className="container">
+                    <Card className={'card'}  style={{ width: '20rem' }}>
+                        <Card.Img className={'fish-img'} variant="top" src={fish} />
+                        <Card.Body>
+                            <Card.Title className={'title'}>Akvaryum Balıkları</Card.Title>
+                            <Card.Text className="text-center">
+                                Akvaryum balıkları, su altında yaşam süren ve çeşitli renk, şekil ve boyutlara sahip olan büyüleyici canlılardır.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="container">
+                    <Card className={'card'}  style={{ width: '20rem' }}>
+                        <Card.Img className={'fish-img'} variant="top" src={omurgasiz} />
+                        <Card.Body>
+                            <Card.Title className={'title'}>Akvaryum Balıkları</Card.Title>
+                            <Card.Text className="text-center">
+                                Akvaryum balıkları, su altında yaşam süren ve çeşitli renk, şekil ve boyutlara sahip olan büyüleyici canlılardır.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="container">
+                    <Card className={'card'}  style={{ width: '20rem' }}>
+                        <Card.Img className={'fish-img'} variant="top" src={herb} />
+                        <Card.Body>
+                            <Card.Title className={'title'}>Akvaryum Balıkları</Card.Title>
+                            <Card.Text className="text-center">
+                                Akvaryum balıkları, su altında yaşam süren ve çeşitli renk, şekil ve boyutlara sahip olan büyüleyici canlılardır.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+        </div>
+
+    );
+};
 
 export default WriteNew;
